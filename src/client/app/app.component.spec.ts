f04c0b99bb9f10cdc32803478ed777f49ae00cd2
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2016-11-28 14:45:16 -0500

    fix test toolbar->header

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { MapComponent } from './map/map.component';
-import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 export function main() {
@@ -30,7 +30,7 @@ export function main() {
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [FormsModule, RouterTestingModule.withRoutes(config)],
-        declarations: [TestComponent, ToolbarComponent, MapComponent,
+        declarations: [TestComponent, HeaderComponent, MapComponent,
           NavbarComponent, AppComponent,
           HomeComponent, AboutComponent],
         providers: [
