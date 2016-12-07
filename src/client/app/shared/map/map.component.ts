52e6ad13c6cdab3e126482ccf78ebaaa927b73fc
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2016-12-07 14:54:59 -0500

    map with params

--- src/client/app/shared/map/map.component.ts
+++ src/client/app/shared/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, OnInit, EventEmitter, AfterViewInit, AfterContentInit } from '@angular/core';
-
+import { MapService } from "./map.service";
 declare let ol: any;
 
 /*declare var module: {
@@ -19,35 +19,51 @@ export class MapComponent implements AfterViewInit, OnInit {
   @Output() mapCreated = new EventEmitter();
   @Output() sidebarToggled = new EventEmitter();
 
-  map: any; // Ng2ol3Map;
+  map: any;
   view: any; // ol.View;
   target: string = 'div-map';
   hasSidebar: boolean;
   zoomDuration: number = 500;
   sidebarCollapsible: boolean = false;
 
-  constructor() {}
+  constructor(private mapService: MapService) {}
 
   public ngOnInit(): any {
-    /*
-    this.hasSidebar = this.options.hasOwnProperty("sidebar");
-    this.sidebarCollapsible = (this.hasSidebar && this.options.sidebar.hasOwnProperty("collapsible")) ? this.options.sidebar.collapsible : false;
-    this.target = this.options.map.target;
-    */
-    this.view = new ol.View({
-      projection: /*this.options.map.view.projection ||*/ 'EPSG:900913',
-      center: /*this.options.map.view.center ||*/ ol.proj.fromLonLat([-73.5, 47.168464955013], "EPSG:900913"),
-      zoom: /*this.options.map.view.zoom ||*/ 7
-    });
+    this.mapService.get()
+      .subscribe(
+        res => this.initMap(res),
+        // error => this.errorMessage = <any>error
+      );
+
   }
 
   public ngAfterViewInit(): any {
-    this.map = new ol.Map({
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
+
+  }
+
+  private initMap(options: any): any {
+    this.view = new ol.View({
+      projection: options.view.projection || 'EPSG:3857',
+      center: options.view.center || ol.proj.fromLonLat([-73.5, 47.168464955013], "EPSG:900913"),
+      zoom: options.view.zoom || 7
+    });
+
+    let layers = [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      }),
+      new ol.layer.Image({
+        // extent: [-13884991, 2870341, -7455066, 6338219],
+        source: new ol.source.ImageWMS({
+          url: options.layers[0].url,
+          params: {'LAYERS': options.layers[0].name},
+          // serverType: 'geoserver'
         })
-      ],
+      })
+    ];
+
+    this.map = new ol.Map({
+      layers: layers,
       target: this.target,
       controls: ol.control.defaults({
         attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
@@ -57,4 +73,6 @@ export class MapComponent implements AfterViewInit, OnInit {
       view: this.view
     });
   }
+
+
 }
